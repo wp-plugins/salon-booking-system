cf4727390340341a745196cf51f3887b69da7427
wordpresschef <wordpresschef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-13 20:39:52 +0000

    importo la versione 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/salon-booking-system/trunk@1159895 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/admin/_calendar_title.php
+++
@@ -1,4 +0,0 @@
-<?php echo $booking->getDisplayName()?>
- <?php echo $booking->getStartsAt()->format('d/m/Y h:i') ?>
- (<?php echo implode(', ',$booking->getServices()) ?>)
-
--- views/shortcode/_errors.php
+++
@@ -1,9 +0,0 @@
-<?php if ($errors): ?>
-    <div class="alert alert-danger">
-        <ul>
-            <?php foreach ($errors as $error): ?>
-                <li><?php echo $error ?></li>
-            <?php endforeach ?>
-        </ul>
-    </div>
-<?php endif ?>
