f56e2d03085a1011c6891a394524bc30f368494b
wordpresschef <wordpresschef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-13 20:13:48 +0000

    importo la versione 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/salon-booking-system/tags/views@1159876 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ shortcode/salon_attendant.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @var SLN_Plugin                        $plugin
+ * @var string                            $formAction
+ * @var string                            $submitName
+ * @var SLN_Shortcode_Salon_AttendantsStep $step
+ */
+$bb = $plugin->getBookingBuilder();
+$attendants = $step->getAttendants();
+?>
+<h1><?php _e('Select your attendant','sln')?></h1>
+<form id="salon-step-secondary" method="post" action="<?php echo $formAction ?>" role="form">
+    <?php include "_attendants.php"; ?>
+    <?php include "_form_actions.php" ?>
+</form>
