7e3c97c347985631158fd3e6f97dce28105d03c8
wordpresschef <wordpresschef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-22 12:04:06 +0000

    elimino il vecchio trunk
    git-svn-id: https://plugins.svn.wordpress.org/salon-booking-system/trunk@1165679 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- src/SLN/Helper/AvailabilityFuncs.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class SLN_Helper_AvailabilityFuncs
-{
-} 
--- views/admin/_calendar_event.php
+++
@@ -1,3 +0,0 @@
-<span class="name"><?php echo $booking->getDisplayName()?></span>|
-<span class="date"><?php echo $booking->getStartsAt()->format('d/m/Y') ?></span>|
-<span class="time"><?php echo $booking->getStartsAt()->format('H:i') ?></span>
