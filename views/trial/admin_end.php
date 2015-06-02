65652c43fd53766b9da051a01b6ac831bb21bba1
wordpresschef <wordpresschef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-02 09:23:32 +0000

    cancello
    git-svn-id: https://plugins.svn.wordpress.org/salon-booking-system/trunk@1172598 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/trial/admin_end.php
+++
@@ -1,5 +0,0 @@
-<div id="sln-setting-error" class="updated error">
-    <h3><?php _e('Salon booking plugin is expired','sln') ?></h3>
-
-    <?php _e('<p>Update your plugin to a premium version</p>','sln');?>
-</div>
