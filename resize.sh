275e54e2444dc8e71624aaefb99020660ed052c3
wordpresschef <wordpresschef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-07 08:33:09 +0000

    elimino
    git-svn-id: https://plugins.svn.wordpress.org/salon-booking-system/tags/1.0.5@1193908 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- resize.sh
+++
@@ -1,2 +0,0 @@
-convert img/admin_icon_full.png  -resize 20x20\! img/admin_icon.png
-convert img/settings_icon_full.png  -resize 20x20\! img/settings_icon.png
--- updatepo.sh
+++
@@ -1,7 +0,0 @@
-find . -iname "*.php" > /tmp/sln_file_list.txt
-xgettext --from-code=utf-8 -d sln -j -f /tmp/sln_file_list.txt --keyword=__ --keyword=_e -o languages/sln-en_EN.po
-xgettext --from-code=utf-8 -d sln -j -f /tmp/sln_file_list.txt --keyword=__ --keyword=_e -o languages/sln-en_US.po
-xgettext --from-code=utf-8 -d sln -j -f /tmp/sln_file_list.txt --keyword=__ --keyword=_e -o languages/sln-it_IT.po
-xgettext --from-code=utf-8 -d sln -j -f /tmp/sln_file_list.txt --keyword=__ --keyword=_e -o languages/sln-de_DE.po
-xgettext --from-code=utf-8 -d sln -j -f /tmp/sln_file_list.txt --keyword=__ --keyword=_e -o languages/sln-fr_FR.po
-xgettext --from-code=utf-8 -d sln -j -f /tmp/sln_file_list.txt --keyword=__ --keyword=_e -o languages/sln-es_ES.po
