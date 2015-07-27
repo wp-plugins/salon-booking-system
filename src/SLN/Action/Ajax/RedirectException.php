0802868c791466626a4ce76b393e7e9474220974
wordpresschef <wordpresschef@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-27 10:24:17 +0000

    elimino vecchia versione
    git-svn-id: https://plugins.svn.wordpress.org/salon-booking-system/trunk@1207387 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- src/SLN/Action/Ajax/RedirectException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * marino: yes it's dirty and i like it:)
- */
-class SLN_Action_Ajax_RedirectException extends Exception{
-
-}
--- views/trial/admin_end.php
+++
@@ -1,5 +0,0 @@
-<div id="sln-setting-error" class="updated error">
-    <h3><?php _e('Salon booking plugin is expired','sln') ?></h3>
-
-    <?php _e('<p><a href="http://plugins.wordpresschef.it/downloads/salon-booking-wordpress-plugin/">Please upgrade Salon Booking to a Premium version</a></p>','sln');?>
-</div>
